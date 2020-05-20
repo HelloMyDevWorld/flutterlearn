ec82876ebcd95e352d5f5274495afe8a4830f2bd
Patryk Sadowski <pasad@ciklum.com>
2020-05-20 09:09:08 +0200

    ADD IOS / ANDROID location app

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'first_app/first_app.dart';
 import 'meals/meals_main.dart';
 import 'quize/main_quize.dart';
 import 'shop/shop.dart';
+import 'places/main_places.dart';
 
 // void main() => runApp(FirstApp());
 
@@ -13,4 +14,6 @@ import 'shop/shop.dart';
 
 // void main() => runApp(MealsApp());
 
-void main() => runApp(ShopApp());
+// void main() => runApp(ShopApp());
+
+void main() => runApp(Places());
---
+++ lib/places/main_places.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './providers/great_places.dart';
+import './screens/places_list_screen.dart';
+import './screens/add_place_screen.dart';
+import './screens/place_detail_screen.dart';
+
+class Places extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+          title: 'Great Places',
+          theme: ThemeData(
+            primarySwatch: Colors.indigo,
+            accentColor: Colors.amber,
+          ),
+          home: PlacesListScreen(),
+          routes: {
+            AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+            PlaceDetailScreen.routeName: (ctx) => PlaceDetailScreen(),
+          }),
+    );
+  }
+}
